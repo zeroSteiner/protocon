123278765a9e2acab369ece3092b92d39dbd33fb
William Vu <5082238+wvu@users.noreply.github.com>
2020-01-24 13:51:18 -0600

    Once more, with feeling

--- lib/protocon/plugins/driver_tcp.py
+++ lib/protocon/plugins/driver_tcp.py
@@ -96,15 +96,16 @@ class ConnectionDriver(protocon.ConnectionDriver):
 			self._addrinfo = self._addrinfo._replace(sockaddr=self._addrinfo.sockaddr[:3] + (scope_id,))
 
 		tcp_sock = socket.socket(self._addrinfo.family, self._addrinfo.type)
-		context = ssl.create_default_context()
-		context.check_hostname = False
-		context.verify_mode = ssl.CERT_NONE
 		if self.settings['type'] == 'client':
-			ssl_sock = context.wrap_socket(tcp_sock)
-			ssl_sock.connect(self._addrinfo.sockaddr)
-			self._connection = ssl_sock
+			if self.url.scheme.startswith('ssl'):
+				context = ssl.create_default_context()
+				context.check_hostname = False
+				context.verify_mode = ssl.CERT_NONE
+				tcp_sock = context.wrap_socket(tcp_sock)
+			tcp_sock.connect(self._addrinfo.sockaddr)
+			self._connection = tcp_sock
 		elif self.settings['type'] == 'server':
-			if 'ssl' in self.url.scheme:
+			if self.url.scheme.startswith('ssl'):
 				raise protocon.ProtoconDriverError("{0} does not support server".format(self.url.scheme))
 			tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			tcp_sock.bind(self._addrinfo.sockaddr)
