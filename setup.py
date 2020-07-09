9c814765625e694e34582ad157cc579871665e77
Spencer McIntyre <zeroSteiner@gmail.com>
2020-07-08 20:13:29 -0400

    Drop support for Python 3.4, it's old

--- setup.py
+++ setup.py
@@ -82,9 +82,10 @@ setup(
 		'Intended Audience :: System Administrators',
 		'License :: OSI Approved :: BSD License',
 		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
 		'Topic :: Security',
 		'Topic :: Software Development :: Libraries :: Python Modules'
 	],
