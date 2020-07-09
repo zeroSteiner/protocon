43222f717fb9e5806f7f53f7c360723d84e65147
Spencer McIntyre <zeroSteiner@gmail.com>
2020-07-08 20:11:13 -0400

    Bump the version to 1.3.1

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ through simple send and receive transcripts.\
 
 setup(
 	name='protocon',
-	version='1.3.0',
+	version='1.3.1',
 	author='Spencer McIntyre',
 	author_email='zeroSteiner@gmail.com',
 	maintainer='Spencer McIntyre',
